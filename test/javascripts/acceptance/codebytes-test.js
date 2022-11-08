6a24e37476c31141151fcb88479731f26081eac4
Penar Musaraj <pmusaraj@gmail.com>
2022-11-08 13:42:51 -0500

    FIX: Tests failing due to changing defaults in core (#9)
    Needed to explicitly mark `allow_uncategorized_topics: true`.

--- test/javascripts/acceptance/codebytes-test.js
+++ test/javascripts/acceptance/codebytes-test.js
@@ -1,20 +1,21 @@
-import { 
-  acceptance, 
-  exists, 
+import {
+  acceptance,
+  exists,
   queryAll,
-  visible 
+  visible
 } from "discourse/tests/helpers/qunit-helpers";
 import { test } from "qunit";
 import { click, currentURL, fillIn, triggerEvent, visit } from "@ember/test-helpers";
 
 acceptance("CodeBytes", function (needs) {
   needs.user();
-  needs.settings({ code_bytes_enabled: true });
+  needs.settings({ code_bytes_enabled: true, allow_uncategorized_topics: true });
+  // needs.settings({ enable_whispers: true,  });
 
   test("it inserts a codebyte when the Create a Codebyte composer toolbar button is clicked", async function (assert) {
     await visit("/");
     await click("#create-topic");
-    
+
     await click(".d-editor button.codecademy-codebyte-discourse-btn");
     assert.equal(
       queryAll(".d-editor-input").val(),
@@ -40,7 +41,7 @@ acceptance("CodeBytes", function (needs) {
   test("it renders an iframe in the preview area", async function (assert) {
     await visit("/");
     await click("#create-topic");
-    
+
     await fillIn(
       ".d-editor-input",
       '[codebyte]\n\n[/codebyte]'
@@ -55,7 +56,7 @@ acceptance("CodeBytes", function (needs) {
   test("it updates the markdown when the iframe sends a save response message", async function (assert) {
     await visit("/");
     await click("#create-topic");
-    
+
     await fillIn(
       ".d-editor-input",
       '[codebyte]\n\n[/codebyte]'
@@ -95,7 +96,7 @@ acceptance("CodeBytes", function (needs) {
   test("it prevents saving if a codebyte is missing the language attribute", async function (assert) {
     await visit("/");
     await click("#create-topic");
-    
+
     await fillIn(
       ".d-editor-input",
       '[codebyte]\n\n[/codebyte]'
@@ -126,5 +127,5 @@ acceptance("CodeBytes", function (needs) {
       "/t/internationalization-localization/280",
       "it successfully creates the post if the codebyte is valid"
     );
-  });  
+  });
 });
